80bbb10244fe821657369c60f8f49705d2fb569e
Truls <38424484+infernuxmonster@users.noreply.github.com>
2023-06-02 10:26:28 +0200

    Update __init__.py

--- AzureFunction/MISP2Sentinel/__init__.py
+++ AzureFunction/MISP2Sentinel/__init__.py
@@ -14,6 +14,7 @@ import azure.functions as func
 import requests
 import json
 
+mispkey=os.getenv('mispkey')
 
 def _get_events():
     misp = ExpandedPyMISP(config.misp_domain, config.misp_key, config.misp_verifycert)
