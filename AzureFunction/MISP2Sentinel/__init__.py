f2b97b6f794b91a52cdf02901b3b48a9aba742b9
infernuxmonster <infernux@infernux.no>
2023-07-07 08:59:54 +0200

    Update init to new version, remove bloat, upd logging

--- AzureFunction/MISP2Sentinel/__init__.py
+++ AzureFunction/MISP2Sentinel/__init__.py
@@ -14,137 +14,105 @@ import azure.functions as func
 import requests
 import json
 
-def _get_events():
-    misp = ExpandedPyMISP(config.misp_domain, config.misp_key, config.misp_verifycert)
-    if len(config.misp_event_filters) == 0:
-        return [event['Event'] for event in misp.search(controller='events', return_format='json')]
-    events_for_each_filter = [
-        [event['Event'] for event in misp.search(controller='events', return_format='json', **config.misp_event_filters)]
-    ]
-    event_ids_for_each_filter = [set(event['id'] for event in events) for events in events_for_each_filter]
-    event_ids_intersection = reduce((lambda x, y: x & y), event_ids_for_each_filter)
-    return [event for event in events_for_each_filter[0] if event['id'] in event_ids_intersection]
+if config.misp_verifycert is False:
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+def _get_misp_events_stix():
+    misp = ExpandedPyMISP(config.misp_domain, config.misp_key, config.misp_verifycert, False)
+    result_set = []
+    logging.debug("Query MISP for events and return them in stix2 format.")
+    remaining_misp_pages = True
+    misp_page = 0
+    empty_event = {"info": "Unknown MISP event", "uuid": "", "tags": [], "tlp": False}
+    misp_tags_ignore = ["Threat-Report", "misp:tool=\"MISP-STIX-Converter\""]
+    misp_indicator_ids = []
 
-def _graph_post_request_body_generator(parsed_events):
-    for event in parsed_events:
-        request_body_metadata = {
-            **{field: event[field] for field in REQUIRED_GRAPH_METADATA},
-            **{field: event[field] for field in OPTIONAL_GRAPH_METADATA if field in event},
-            'action': config.action,
-            'passiveOnly': config.passiveOnly,
-            'targetProduct': config.targetProduct,
-        }
+    while remaining_misp_pages:
+        misp_reports_indicator = {}
+        try:
+            result = misp.search(controller='events', return_format='stix2', **config.misp_event_filters, limit=config.misp_event_limit_per_page, page=misp_page)
+            if result.get("objects", False):
+                logging.debug("Received MISP events page {}".format(misp_page))
+                stix2_objects = result.get("objects")
 
-        if len(request_body_metadata.get('threatType', [])) < 1:
-            request_body_metadata['threatType'] = 'watchlist'
-        if config.defaultConfidenceLevel:
-            request_body_metadata["confidence"] = config.defaultConfidenceLevel
-        for request_object in event['request_objects']:
-            request_body = {
-                **request_body_metadata.copy(),
-                **request_object.__dict__,
-                'tags': request_body_metadata.copy()['tags'] + request_object.__dict__['tags'],
-            }
-            yield request_body
+                for element in stix2_objects:       # Extract event information
+                    if element.get("type", False) == "report":
+                        misp_tags = []
+                        misp_tag_tlp = ""
+                        if element.get("labels", False):
+                            for label in element.get("labels"):
+                                if "tlp:" in label.lower() and label.lower() in TLP_MARKING_OBJECT_DEFINITION:
+                                    misp_tag_tlp = label.lower()
+                                if label not in misp_tags_ignore and label not in misp_tags:
+                                    misp_tags.append(label)
+                        if element.get("object_refs", False):
+                            for object_ref in element.get("object_refs"):
+                                if "indicator--" in object_ref:
+                                    event_info = element.get("name", "").strip()
+                                    event_uuid = element.get("id", "report--000").strip().split("report--")[1]
+                                    misp_reports_indicator[object_ref] = {"info": event_info, "uuid": event_uuid, "tags": misp_tags, "tlp": misp_tag_tlp}
 
+                for element in stix2_objects:
+                    if element.get("type", False) in UPLOAD_INDICATOR_API_ACCEPTED_TYPES and element.get("id") not in misp_indicator_ids:
+                        event_info = misp_reports_indicator.get(element["id"], empty_event)["info"]
+                        event_uuid = misp_reports_indicator.get(element["id"], empty_event)["uuid"]
+                        event_tags = misp_reports_indicator.get(element["id"], empty_event)["tags"]
+                        event_tlp = misp_reports_indicator.get(element["id"], empty_event)["tlp"]
 
-def _handle_timestamp(parsed_event):
-    parsed_event['lastReportedDateTime'] = str(
-        datetime.datetime.fromtimestamp(int(parsed_event['lastReportedDateTime'])))
+                        if event_tlp:
+                            if element.get("object_marking_refs"):
+                                element["object_marking_refs"].append(TLP_MARKING_OBJECT_DEFINITION[event_tlp])
+                            else:
+                                element["object_marking_refs"] = [TLP_MARKING_OBJECT_DEFINITION[event_tlp]]
+                        for tag in event_tags:
+                            if tag not in element.get("labels", []):
+                                element["labels"].append(tag)
 
+                        element["name"] = "{} {}".format(event_info, element.get("name", "")).strip()
 
-def _handle_diamond_model(parsed_event):
-    for tag in parsed_event['tags']:
-        if 'diamond-model:' in tag:
-            parsed_event['diamondModel'] = tag.split(':')[1]
+                        # Check for misp:confidence-level="fairly-confident" and others
+                        if not element.get("confidence", False):
+                            element["confidence"] = config.default_confidence
 
+                        # Link to MISP event
+                        misp_event_reference = {
+                            "source_name": "MISP",
+                            "description": "MISP Event: {}".format(event_info),
+                            "external_id": event_uuid,
+                            "url": "https://{}/events/view/{}".format(config.misp_domain, event_uuid)
+                        }
+                        if element.get("external_references", False):
+                            element["external_references"].append(misp_event_reference)
+                        else:
+                            element["external_references"] = [misp_event_reference]
 
-def _handle_tlp_level(parsed_event):
-    for tag in parsed_event['tags']:
-        if 'tlp:' in tag:
-            parsed_event['tlpLevel'] = tag.split(':')[1].lower().capitalize()
-        if parsed_event['tlpLevel'] == 'Clear':
-            parsed_event['tlpLevel'] = 'White'
-    if 'tlpLevel' not in parsed_event:
-        parsed_event['tlpLevel'] = 'Red'
+                        misp_indicator_ids.append(element.get("id"))
+                        result_set.append(element)
+                logging.info("Processed {} indicators.".format(len(result_set)))
+                misp_page += 1
+        except Exception as e:
+            remaining_misp_pages = False
+            logging.info("Finished receiving MISP events.")
 
-def push_to_sentinel(tenant, id, secret):
-    config.graph_auth[TENANT] = tenant
-    config.graph_auth[CLIENT_ID] = id
-    config.graph_auth[CLIENT_SECRET] = secret
-    if '-r' in sys.argv:
-        print("Retrieve indicators from Sentinel")
-        RequestManager.read_tiindicators()
-        sys.exit()
+    return result_set, len(result_set)
 
-    if '-d' in sys.argv:
-        print("Delete indicators from Sentinel")
-        # Delete an indicator
-        request_manager = RequestManager(0)
-        access_token = request_manager._get_access_token(
-            config.graph_auth[TENANT],
-            config.graph_auth[CLIENT_ID],
-            config.graph_auth[CLIENT_SECRET])
-        headers = {"Authorization": f"Bearer {access_token}", 'user-agent': 'MISP/1.0'}
-        request_body = {'value': [sys.argv[2]]}
-        response = requests.post(GRAPH_BULK_DEL_URL, headers=headers, json=request_body).json()
-        print(json.dumps(response, indent=2))
-        sys.exit()
-
-    config.verbose_log = ('-v' in sys.argv)
-    print('fetching & parsing data from misp...')
-    events = _get_events()
-    parsed_events = list()
-    for event in events:
-        parsed_event = defaultdict(list)
-
-        for key, mapping in EVENT_MAPPING.items():
-            parsed_event[mapping] = event.get(key, "")
-
-        # Tags on event level
-        tags = []
-        for tag in event.get("Tag", []):
-            if 'sentinel-threattype' in tag['name']:    # Can be overriden on attribute level
-                parsed_event['threatType'] = tag['name'].split(':')[1]
-                continue
-            if config.misp_ignore_localtags:
-                if tag["local"] != 1:
-                    tags.append(tag['name'].strip())
-        parsed_event['tags'] = tags
-        _handle_diamond_model(parsed_event)
-        _handle_tlp_level(parsed_event)
-        _handle_timestamp(parsed_event)
-
-        for attr in event['Attribute']:
-            if attr['type'] == 'threat-actor':
-                parsed_event['activityGroupNames'].append(attr['value'])
-            if attr['type'] == 'comment':
-                parsed_event['description'] += attr['value']
-            if attr['type'] in MISP_ACTIONABLE_TYPES:
-                parsed_event['request_objects'].append(RequestObject(attr))
-        for obj in event['Object']:
-            for attr in obj['Attribute']:
-                if attr['type'] == 'threat-actor':
-                    parsed_event['activityGroupNames'].append(attr['value'])
-                if attr['type'] == 'comment':
-                    parsed_event['description'] += attr['value']
-                if attr['type'] in MISP_ACTIONABLE_TYPES:
-                    parsed_event['request_objects'].append(RequestObject(attr))
-        parsed_events.append(parsed_event)
-    del events
-    total_indicators = sum([len(v['request_objects']) for v in parsed_events])
+def push_to_sentinel(tenant, id, secret, workspace):
+    logging.info(f"Using Microsoft Upload Indicator API")
+    config.ms_auth[TENANT] = tenant
+    config.ms_auth[CLIENT_ID] = id
+    config.ms_auth[CLIENT_SECRET] = secret
+    config.ms_auth[WORKSPACE_ID] = workspace
+    parsed_indicators, total_indicators = _get_misp_events_stix()
+    logging.info("Found {} indicators in MISP".format(total_indicators))
 
     with RequestManager(total_indicators, tenant) as request_manager:
-        for request_body in _graph_post_request_body_generator(parsed_events):
-            if config.verbose_log:
-                print(f"request body: {request_body}")
-            request_manager.handle_indicator(request_body)
+        request_manager.upload_indicators(parsed_indicators)
 
 def pmain():
     tenants = json.loads(os.getenv('tenants'))
     for key, value in tenants.items():
-        push_to_sentinel(key, value['id'], value['secret'])
+        push_to_sentinel(key, value['id'], value['secret'], value['workspaceid'])
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -153,5 +121,7 @@ def main(mytimer: func.TimerRequest) -> None:
     if mytimer.past_due:
         logging.info('The timer is past due!')
 
+    logging.info("Start MISP2Sentinel")
     pmain()
+    logging.info("End MISP2Sentinel")
     logging.info('Python timer trigger function ran at %s', utc_timestamp)
