46916d2abc1e23087506ee7a8a93ae90af90038e
infernuxmonster <infernux@infernux.no>
2023-07-18 11:36:23 +0200

    Update config.py with changes from cudeso

--- AzureFunction/MISP2Sentinel/config.py
+++ AzureFunction/MISP2Sentinel/config.py
@@ -2,6 +2,11 @@ import os
 mispkey=os.getenv('mispkey')
 mispurl=os.getenv('mispurl')
 
+#####################
+# Microsoft Section #
+#####################
+
+# MS API settings
 ms_auth = {
     'tenant': '',
     'client_id': '',
@@ -10,25 +15,57 @@ ms_auth = {
     'graph_api': False,
     'workspace_id': ''
 }
+
 ms_max_indicators_request = 100     # Throttle max: 100 indicators per request
 ms_max_requests_minute = 100        # Throttle max: 100 requests per minute
 ms_useragent = 'MISP-1.0'
 ms_target_product = 'Azure Sentinel'    # targetProduct
+ms_api_version = "2022-07-01"       # Upload Indicators API version
+
+# Graph API only settings
 ms_passiveonly = False                  # passiveOnly
 ms_action = 'alert'                     # action
 
-misp_event_filters = {
-    "timestamp": "1d"
-}
+################
+# MISP Section #
+################
 
+# MISP API settings
 misp_key = mispkey
 misp_domain = mispurl
 misp_verifycert = False
+
+# MISP Event filters
+misp_event_filters = {
+    "timestamp": "1d",
+    "enforceWarninglist": True,
+    "includeEventTags": True,
+}
+
+# MISP pagination settings
 misp_event_limit_per_page = 50      # Limit memory use when querying MISP for STIX packages
 
-days_to_expire = 30
+########################
+# Integration settings #
+########################
+
+log_file = "/tmp/misp2sentinel.log"
+verbose_log = False
+write_parsed_indicators = False      # Upload Indicators only
+
+# Graph API only settings
+ignore_localtags = True 
 network_ignore_direction = True
-verbose_log = True
-write_post_json = False
-misp_ignore_localtags = True
+write_post_json = False 
+
+
+# IOC settings
 default_confidence = 50
+days_to_expire = 30
+days_to_expire_start = "current_date" # Upload Indicators API only. Start counting from "valid_from" | "current_date" ; 
+days_to_expire_mapping = {          # Upload indicators API only. Mapping for expiration of specific indicator types
+                    "ipv4-addr": 150,
+                    "ipv6-addr": 150,
+                    "domain-name": 300,
+                    "url": 400,
+                }
